450db02c18794e92a43cddfb6854be1f48991649
andydlindsay@gmail.com <andydlindsay@gmail.com>
2020-09-13 22:39:24 -0700

    unignore buld folder

---
+++ client/build/precache-manifest.2900429ad1667bc188d6ce28586159c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0325d0604b2cde8ee5e7d1690f7bd9f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcf6921167c5c8a4b3ce",
+    "url": "/static/css/main.356830eb.chunk.css"
+  },
+  {
+    "revision": "21f2b8d253e72bdf556e",
+    "url": "/static/js/2.64c6c57d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.64c6c57d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcf6921167c5c8a4b3ce",
+    "url": "/static/js/main.3a4f3e91.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
